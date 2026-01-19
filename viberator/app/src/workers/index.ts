d388fc77b466da7d67bc0b8993267cd52b6b588f
Jussi Hallila <jussi@hallila.com>
2026-01-19 20:43:54 +0100

    feat(03-02): export CredentialProvider and ConfigLoader from workers module
    - Export CredentialProvider from './CredentialProvider'
    - Export ConfigLoader and InstructionFile from './ConfigLoader'
    - Keep existing exports (CallbackClient, ViberatorWorker, types)

--- viberator/app/src/workers/index.ts
+++ viberator/app/src/workers/index.ts
@@ -1,3 +1,5 @@
 export { CallbackClient } from './CallbackClient';
 export { ViberatorWorker } from './viberator';
 export * from './types';
+export { CredentialProvider } from './CredentialProvider';
+export { ConfigLoader, InstructionFile } from './ConfigLoader';
